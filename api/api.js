8b93a7fdd82a63bd53bb95d8bb734baae2c1957a
Eddy Mercado <Nerdboy1701@outlook.com>
2021-05-13 21:45:49 -0400

    Create Artist Route

--- api/api.js
+++ api/api.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const artistRouter = require('./artists');
 
 const apiRouter = express.Router();
+apiRouter.use('/artist, artistRouter');
 
 module.exports = apiRouter;
